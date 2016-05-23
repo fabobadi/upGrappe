d4d8e033e38d9703e00ba3dc3062786a6ac60c63
fabobadi <fabobadi@uc.cl>
2016-05-23 10:41:46 -0400

    addapted components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
-import React, {Proptypes, Component, View } from 'react';
-import {AppRegistry, Navigator, StyleSheet, Text, BackAndroid } from 'react-native'
-import {Scene, Reducer, Router, Switch, TabBar, Modal, Schema, Actions} from 'react-native-router-flux'
+import React, { Component } from 'react';
+import {View, StyleSheet, BackAndroid } from 'react-native';
+import {Scene,  TabBar, Router, Actions } from 'react-native-router-flux';
 
+import Platform from './utils/platform';
 
-import PageOne from './components/PageOne';
-import PageTwo from './components/PageTwo';
+import Activities from './components/Activities';
+import Analytics from './components/Analytics';
 
 export default class Routing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
     };
+  }
+
+  componentDidMount() {
+    if (Platform.Android && !this.state.mounted) {
+      BackAndroid.addEventListener('hardwareBackPress', this.goBack);
+    }
+  }
+
+  goBack() {
+    try {
+      Actions.pop();
+      return true;
+    } catch (err) {
+      BackAndroid.exitApp();
+      return true;
+    }
   }
 
   render() {
     return (
       <Router>
         <Scene key="root">
-            <Scene key="pageOne" component={PageOne} title="PageOne" initial/>
-            <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
+          <Scene
+            key="main"
+            initial
+            tabs
+            hideNavBar
+            default="atlasses"
+            direction="vertical"
+          >
+            <Scene key="activities" component={Activities} title="Activities"/>
+            <Scene key="analytics" component={Analytics} title="Analytics"/>
+          </Scene>
         </Scene>
       </Router>
     );
@@ -30,14 +55,14 @@ const styles = StyleSheet.create({
   tabbar: {
     borderTopWidth: 0.5,
     borderTopColor: 'grey',
-    backgroundColor: '#F9F9F9',
+    backgroundColor: '#F9F9F9'
   },
   transparent: {
     backgroundColor: 'transparent',
     borderBottomWidth: 0,
-    borderBottomColor: 'transparent',
+    borderBottomColor: 'transparent'
   },
   title: {
-    color: 'white',
-  },
+    color: 'white'
+  }
 });
